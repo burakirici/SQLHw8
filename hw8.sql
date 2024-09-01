fb00b239cd644b8999fe6f8eeca5030d23cae81e
Burak Kırıcı <burakpinar76@gmail.com>
2024-09-01 14:55:55 +0300

    Add files via upload

---
+++ hw8.sql
@@ -0,0 +1,45 @@
+-- Question 1
+--CREATE TABLE employee (
+--    id INTEGER PRIMARY KEY,
+--   name VARCHAR(50),
+--    birthday DATE,
+--    email VARCHAR(100)
+--);
+
+-- Question 2 (Sample)
+-- insert into employee (id, name, birthday, email) values (1, 'Yovonnda di Rocca', '7/7/2024', 'ydi0@eventbrite.com');
+
+-- Question 3
+
+--UPDATE employee
+--SET name = 'John Doe', birthday = '1985-07-15', email = 'john.doe@example.com'
+--WHERE id = 1;
+
+--UPDATE employee
+--SET birthday = '1990-01-20', email = 'jane.smith@example.com'
+--WHERE name = 'Jane Smith';
+
+--UPDATE employee
+--SET name = 'Michael Johnson', email = 'michael.johnson@example.com'
+--WHERE birthday = '1982-03-05';
+
+--UPDATE employee
+--SET name = 'Emily Davis', birthday = '1995-09-30'
+--WHERE email = 'emily.davis@example.com';
+
+-- Question 4
+
+--DELETE FROM employee
+--WHERE id = 1;
+
+--DELETE FROM employee
+--WHERE name = 'Jane Smith';
+
+--DELETE FROM employee
+--WHERE birthday = '1982-03-05';
+
+--DELETE FROM employee
+--WHERE email = 'emily.davis@example.com';
+
+--DELETE FROM employee
+--WHERE id = 2;
